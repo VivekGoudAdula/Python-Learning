bc747e04eb173c83b27bf9d7b11e2382a4c7d2b8
Vivek Goud Adula <adulavivekgoud@gmail.com>
2025-10-09 19:50:48 +0530

    data types

---
+++ 4.data_types.py
@@ -0,0 +1,24 @@
+# Variables can store data of different types, and different types can do different things.
+
+# Text Type: str
+name = "Vivek Goud Adula"
+
+# Numeric Types: int, float, complex
+a = 19         # int
+b = 12.50      # float
+c = 3j         # complex
+
+# Sequence Types: list, tuple, range
+fruits = ["apple", "banana"] # list
+price = (20,30)     # tuple
+numbers = range(5) # range
+
+# Mapping Type: dict
+details = {"name": "Vivek", "age": 19} # dict
+
+# Set Types: set, frozenset
+colors = {"red", "green", "blue"} # set
+frozen_colors = frozenset({"red", "green", "blue"}) # frozenset
+
+# Boolean Type: bool
+is_student = True # bool

f54c08475c8b863143ee33b5b1b385de9cd258ba
Vivek Goud Adula <adulavivekgoud@gmail.com>
2025-10-09 20:04:17 +0530

    casting

---
+++ 5.casting.py
@@ -0,0 +1,17 @@
+# CASTING
+# You can specify the data type of a variable by casting.
+
+# INT
+x = int(1) # x will be an integer of value 1
+y = int(2.9) # y will be an integer of value 2
+z = int("3") # z will be an integer of value 3
+
+# FLOAT
+a = float(1) # a will be a float of value 1.0
+b = float(2.9) # b will be a float of value 2.9
+c = float("3") # c will be a float of value 3.0
+
+# STR
+p = str(10) # p will be a string of value '10'
+q = str(10.01) # q will be a string of value '10.01'
+r = str("Vivek") # r will be a string of value 'Vivek'

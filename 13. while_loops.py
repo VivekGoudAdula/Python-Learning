e20d626e4bc98525cdb7faed4358d8e7111d347e
Vivek Goud Adula <adulavivekgoud@gmail.com>
2025-10-10 22:33:06 +0530

    while-loops

---
+++ 13. while_loops.py
@@ -0,0 +1,23 @@
+# WHILE LOOPS
+# While loop will continue to execute as long as the condition is true
+
+i=1
+while i<5:
+    print(i)
+    i+=1
+
+# # BREAK STATEMENT
+while i<10:
+    i+=1
+    if i ==5:
+        break
+    print(i)
+
+# CONTINUE STATEMENT
+a = 1
+while a<5:
+    a+=1
+    if a == 2:
+        continue
+    print(a)
+

1d5562a07f6c2cd92b1a42d5454a1ba496b34b82
Vivek Goud Adula <adulavivekgoud@gmail.com>
2025-10-16 21:28:14 +0530

    functions

--- 15. functions.py
+++ 15. functions.py
@@ -51,3 +51,88 @@ def details(num,aadhar):
 det1 = details(9999999999,6969696969696969)
 det2 = details(8888888888,9090909090909090)
 print(det1, det2)
+
+
+# MIXING POSITIONAL AND KEYWORD ARGUMENTS
+
+def animal(breed,name,age):
+    print("I have a",age,"year old",breed,"his name is",name)
+animal("Shihtzu","Max",age=9)
+
+
+# POSITIONAL ONLY ARGUMENTS
+def man(a,/):
+    print(a)
+man("ram")
+# man(a="ram") # WILL GIVE AN ERROR
+
+
+# KEYWORD ONLY ARGUMENTS
+def woman(*,b):
+    print(b)
+woman(b="rani")
+# woman("rani") # WILL GIVE AN ERROR
+
+
+# COMBINING POSITTION-ONLY AND KEYWORD-ONLY ARGUMENTS
+def func(w,x,/,*,y,z):
+    return w+x+y+z
+result = func(1,2,y=3,z=4)
+print(result)
+
+
+# -------------------------------
+
+# *args, **kwargs
+
+# ARBITRARY ARGUMENTS - *args
+# The *args parameter allows a function to accept any number of positional arguments.
+# Inside the function, args becomes a tuple containing all the passed arguments
+
+def fruits(*args):
+    print("Type:",type(args))
+    print("All Arguments:",args)
+    print("First fruit:",args[0])
+    print("Second fruit:",args[1])
+fruits("Apple","Mango","Grapes")
+
+
+# ARBITRARY KEYWORD ARGUMENTS - **kwargs
+# The **kwargs parameter allows a function to accept any number of keyword arguments.
+# Inside the function, kwargs becomes a dictionary containing all the keyword arguments:
+
+def details(**info):
+    print("Type:",type(info))
+    print("All Arguments:",info)
+    print("First Name:",info["fName"])
+    print("Last Name:",info["lName"])
+details(lName="Goud",fName="Vivek", city="Hyderabad")
+
+# -------------------------------
+
+# COMBINING *args and **kwargs
+# ORDER MUST BE
+        # regular parameters
+        # *args
+        # **kwargs
+def info(header,*args,**kwargs):
+    print(header)
+    print(args)
+    print(kwargs)
+info("MY INFORMATION","AIML", "III-A", age = "19", Area = "Sainikpuri")
+
+
+# -------------------------------
+
+# UNPACKING LISTS WITH *
+def addition(a,b,c):
+    return a+b+c
+nums=[2,3,4]
+result=addition(*nums)
+print(result)
+
+# UNPACKING DICTIONARIES WITH **
+def person(fname,lname):
+    print("Helloo",fname,lname)
+information = {"lname":"Goud","fname":"Vivek"}
+person(**information)

99b764c4684f1aa1b25cb2c0aa8649c5465545f5
Vivek Goud Adula <adulavivekgoud@gmail.com>
2025-10-20 11:25:27 +0530

    math

---
+++ 29. math.py
@@ -0,0 +1,39 @@
+# MATH
+# This module llows us to perform mathematical tasks on numbers
+import math
+
+# min() and max() FUNCTIONS
+x = min(1,6,0,-1)
+y = max(1,6,0,-1)
+print(x)
+print(y)
+
+# abs() FUNCTION
+z = abs(-8.99283)
+print(z)
+
+# pow() FUNCTION
+a = pow(2,4)
+print(a)
+
+# ----------------------------------------------------------
+
+# MATH MODULE
+
+# sqrt() FUNCTION
+b = math.sqrt(16)
+print(b)
+
+# ceil() FUNCTION
+c = math.ceil(9.1) # Rounds up to the nearest integer
+print(c)
+
+# floor() FUNCTION
+d = math.floor(9.9) # Rounds down to the nearest integer
+print(d)
+
+# pi CONSTANT
+d = math.pi
+print(d)
+
+# ----------------------------------------------------------

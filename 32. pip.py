4373e420d8ecd4982ec93368cc568643a56d0b59
Vivek Goud Adula <adulavivekgoud@gmail.com>
2025-10-20 12:20:45 +0530

    pip

---
+++ 32. pip.py
@@ -0,0 +1,41 @@
+# PIP
+# Python Installation Package
+# pip is a package manager for python packages
+
+# ---------------------------------------------------------
+
+# PACKAGE
+# Package consists of all the files that is needed for a module 
+# Module consists the python code libraries required for your project
+
+# ---------------------------------------------------------
+
+# CHECK PIP VERSION
+# To check the pip version installed on your system, use the following command in the command prompt or terminal
+# pip --version
+
+# ---------------------------------------------------------
+
+# INSTALLING A PACKAGE
+# To install a package using pip, use the following command in the command prompt or terminal
+# pip install package_name
+# Example: pip install camelcase
+
+# ---------------------------------------------------------
+
+import array as arr
+arr = arr.array("i",[1,2,3,4,5])
+print(arr)
+
+# ---------------------------------------------------------
+
+# UNINSTALLING A PACKAGE
+# To uninstall a package using pip, use the following command in the command prompt or terminal
+# pip uninstall package_name
+# Example: pip uninstall camelcase
+
+# ---------------------------------------------------------
+
+# LISTING INSTALLED PACKAGES
+# To list all the installed packages using pip, use the following command in the command prompt or terminal
+# pip list

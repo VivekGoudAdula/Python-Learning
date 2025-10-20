be6d68c1de494da6879f823ec6ffc778458c076b
Vivek Goud Adula <adulavivekgoud@gmail.com>
2025-10-20 16:14:07 +0530

    user input

---
+++ 36. user_input.py
@@ -0,0 +1,20 @@
+# USER INPUT
+
+# ------------------------------------------------------------
+
+# STRING INPUT
+
+name = input("Enter your name: ")
+print(f"Hello {name}")
+
+# MULTIPLE INPUTS
+Class= input("Enter your Class: ")
+section = input("Enter your Section: ")
+print(f"You are in Class {Class} Section {section}")
+
+# ------------------------------------------------------------
+
+# INTEGER INPUT
+
+age = int(input("Enter your age: ")) 
+print(f"Your age is {age}")
